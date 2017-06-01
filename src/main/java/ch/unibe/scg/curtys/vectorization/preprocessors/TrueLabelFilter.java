f663c2398d400c96df87ecd8b62303a9fb028e03
Simon Curty <simon.curty@students.unibe.ch>
2017-06-01 12:53:01 +0200

    adds additional preprocessor

---
+++ src/main/java/ch/unibe/scg/curtys/vectorization/preprocessors/TrueLabelFilter.java
@@ -0,0 +1,29 @@
+package ch.unibe.scg.curtys.vectorization.preprocessors;
+
+import ch.unibe.scg.curtys.vectorization.issue.Issue;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by scurty on 01.06.17.
+ */
+public class TrueLabelFilter extends ChainPreprocessor {
+
+	private final String[] whitelist;
+
+	public TrueLabelFilter(String... labelsWhitelist) {
+		this.whitelist = labelsWhitelist != null ? labelsWhitelist : new String[]{};
+	}
+
+	@Override protected void execute(List<Issue> issues) {
+		issues.removeIf(i -> {
+			for (String label : whitelist) {
+				if (i.getTrueLabel().equals(label))
+					return false;
+			}
+			return true;
+		});
+	}
+}
+
